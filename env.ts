890604ec8bccc300e4ae3a0fdf948d1ca4d85d4b
Fionna <13184582+fionnachan@users.noreply.github.com>
2026-05-18 16:40:36 +0100

    chore: use alchemy rpc (#59)

--- env.ts
+++ env.ts
@@ -8,10 +8,13 @@ export const env = createEnv({
     NEXT_PUBLIC_REOWN_PROJECT_ID: z.string().min(1),
     NEXT_PUBLIC_POSTHOG_TOKEN: z.string().min(1).optional(),
     NEXT_PUBLIC_POSTHOG_HOST: z.string().url().optional(),
+    NEXT_PUBLIC_ALCHEMY_API_KEY: z.string().min(1).optional(),
   },
   runtimeEnv: {
     NEXT_PUBLIC_REOWN_PROJECT_ID: process.env.NEXT_PUBLIC_REOWN_PROJECT_ID,
     NEXT_PUBLIC_POSTHOG_TOKEN: process.env.NEXT_PUBLIC_POSTHOG_TOKEN,
     NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    NEXT_PUBLIC_ALCHEMY_API_KEY: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY,
   },
+  skipValidation: !!process.env.VITEST,
 });
